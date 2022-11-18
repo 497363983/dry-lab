6409af347f42e7c0e381c93db47d386d17d330c4
497363983 <497363983@users.noreply.github.com>
2022-11-18 17:38:33 +0000

    Deploying to pages from @ 497363983/dry-lab@f65f567d6252242ec40e2e987c04abfb49df67a3 🚀

---
+++ assets/about.md.b188e812.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as a}from"./app.5d1fb263.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"about.md","lastUpdated":1668789198000}'),o={name:"about.md"};function r(s,c,n,p,d,_){return a(),e("div")}const f=t(o,[["render",r]]);export{m as __pageData,f as default};
