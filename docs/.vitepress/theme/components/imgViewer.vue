103a578547c312f776593f0dc360bb61382e757d
qww <497363953@qq.com>
2022-11-22 13:34:05 +0800

    feat(component): imgViewer

---
+++ docs/.vitepress/theme/components/imgViewer.vue
@@ -0,0 +1,48 @@
+<script setup>
+defineProps({
+  src: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+    default: "",
+  },
+  width: {
+    type: String,
+    default: "100%",
+  },
+  prefix: {
+    type: String,
+    default: "Fig",
+  },
+  caption: {
+    type: String,
+    default: "",
+  },
+  index: {
+    type: Number,
+    default: null,
+  },
+});
+</script>
+<template>
+  <div class="img-viewer">
+    <img :style="{ width }" :src="src" :alt="alt" />
+    <span v-if="index" v-html="`${prefix} ${index}. ${caption}`"></span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.img-viewer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 1rem 0;
+  img {
+    max-width: 100%;
+    max-height: 100%;
+  }
+}
+</style>
